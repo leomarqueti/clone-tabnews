2e2db45fc717f8a171e0151fdc345ab6f1b40212
Leonardo Marqueti de Lima <94808998+leomarqueti@users.noreply.github.com>
2025-12-07 21:27:27 +0000

    adicionando `docker`

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,5 +1,5 @@
 test("GET to /api/v1/status should return 200", async () => {
-  // precisamos colocar await pois pedimos para o teste esperar um pouco ate a pagina carregar , damos uma promise , uma promessa que jaja chega, entao criamos uma funçao async para que tudo funcione e quando der o a responsa a variavel vai receber no response.status o 200 
+  // precisamos colocar await pois pedimos para o teste esperar um pouco ate a pagina carregar , damos uma promise , uma promessa que jaja chega, entao criamos uma funçao async para que tudo funcione e quando der o a responsa a variavel vai receber no response.status o 200
   const response = await fetch("http://localhost:3000/api/v1/status/");
   expect(response.status).toBe(200);
 });
